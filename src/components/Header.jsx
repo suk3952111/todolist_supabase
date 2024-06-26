196bcbda368879770e975c1e93588cf2f4a742d4
suk3952111 <suk3952111@naver.com>
2024-06-27 03:12:04 +0900

    feat: 로그인 및 회원가입 supabase 연결

---
+++ src/components/Header.jsx
@@ -0,0 +1,78 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import "./Header.css";
+import { useAuthContext } from "../App";
+import { supabase } from "@/main";
+
+function getLinkStyle({ isActive }) {
+  return {
+    textDecoration: isActive ? "underline" : "none",
+  };
+}
+
+const Header = () => {
+  const navigate = useNavigate();
+  const { user, setUser } = useAuthContext();
+
+  const onLogout = async (e) => {
+    e.preventDefault(); // 폼 기본 동작 방지
+
+    try {
+      const { data, error } = await supabase.auth.signOut();
+      console.log("signOut: ", { data, error });
+
+      if (error) {
+        alert(error.message);
+      } else {
+        setUser(null);
+        navigate("/login");
+      }
+    } catch (error) {
+      console.error("로그아웃 중 오류 발생:", error.message);
+      alert(error.message);
+    }
+  };
+
+  return (
+    <header>
+      <nav className="navigator">
+        <div className="navigator-components">
+          <li>
+            <NavLink style={getLinkStyle} to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink style={getLinkStyle} to="/todolist">
+              TodoList
+            </NavLink>
+          </li>
+        </div>
+        <div className="navigator-components">
+          {user ? (
+            <>
+              <li>안녕하세요, {user.email}님</li>
+              <li>
+                <button onClick={onLogout}>로그아웃</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink style={getLinkStyle} to="/login">
+                  로그인
+                </NavLink>
+              </li>
+              <li>
+                <NavLink style={getLinkStyle} to="/signup">
+                  회원가입
+                </NavLink>
+              </li>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import useToggle from "../hooks/useToggle";
+import "./Login.css";
+import { supabase } from "@/main";
+import { useAuthContext } from "../App";
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { setUser } = useAuthContext();
+  const [loginError, setLoginError] = useState("");
+  const [showPassword, toggleShowPassword] = useToggle(false);
+  const navigate = useNavigate();
+
+  const onSubmit = async (formData, e) => {
+    e.preventDefault(); // 폼 기본 동작 방지
+
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: formData.email,
+        password: formData.password,
+      });
+      console.log("signup: ", { data, error });
+      if (error) {
+        setLoginError(error.message);
+      } else {
+        setUser(data);
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("로그인 중 오류 발생:", error.message);
+      setLoginError(error.message);
+    } finally {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <div>
+      <h2>로그인</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>이메일:</label>
+          <input
+            type="text"
+            {...register("email", {
+              required: "Email ID를 입력해주세요",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "올바른 이메일 형식이 아닙니다.",
+              },
+              minLength: {
+                value: 6,
+                message: "이메일은 최소 6자 이상이어야 합니다.",
+              },
+            })}
+          />
+          {errors.email && <p className="error">{errors.email.message}</p>}
+        </div>
+        <div>
+          <label>비밀번호:</label>
+          <div>
+            <input
+              type={showPassword ? "text" : "password"}
+              {...register("password", {
+                required: "비밀번호를 입력해주세요.",
+                minLength: {
+                  value: 8,
+                  message: "비밀번호는 최소 8자 이상이어야 합니다.",
+                },
+              })}
+            />
+          </div>
+          <button type="button" onClick={toggleShowPassword}>
+            {showPassword ? "Hide" : "Show"}
+          </button>
+          {errors.password && (
+            <p className="error">{errors.password.message}</p>
+          )}
+        </div>
+        {loginError && <p className="error">{loginError}</p>}
+        <button type="submit">로그인</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
