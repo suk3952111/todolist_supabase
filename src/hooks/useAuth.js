7e6354cdbeb6ea27da060238bab8e7cc980a0be4
suk3952111 <suk3952111@naver.com>
2024-07-04 19:12:27 +0900

    refactor: 회원가입 로그인 리펙토링

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { supabase } from "@/main";
+
+const useAuth = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      if (session) {
+        setUser(session.user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+  return { user, setUser };
+};
+
+export default useAuth;
